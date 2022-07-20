ab805bcda6ea41571b691f77f747f9f4e7a45a90
유승우 <swyou1123@naver.com>
2022-07-20 15:30:21 +0900

    backend

---
+++ Ecowon-Backend/project/src/main/java/com/ssaft/project/controller/securityController.java
@@ -0,0 +1,35 @@
+package com.ssaft.project.controller;
+
+import com.ssaft.project.Service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/security")
+public class securityController {
+
+
+
+    @Autowired
+    private SecurityService securityService;
+
+    @GetMapping("/create/token")
+    public Map<String, Object> createToken(@RequestParam(value = "subject") String subject){
+        String token = securityService.creatToken(subject, (2*1000*60));
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", token);
+        return map;
+    }
+
+    @GetMapping("/get/subject")
+        public Map<String, Object> getSubject(@RequestParam(value = "token") String token){
+            String subject = securityService.getSubJect(token);
+            Map<String, Object> map = new LinkedHashMap<>();
+            map.put("result", subject);
+            return map;
+        }
+    }
+
