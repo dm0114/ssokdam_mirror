14f170dfdbcb6b92c699f69441814016e8a30e67
kiki249 <kiki249049@gmail.com>
2022-07-28 17:55:31 +0900

    admin

--- Frontend/src/Pages/Admin/AdminHome.jsx
+++ Frontend/src/Pages/Admin/AdminHome.jsx
@@ -1,16 +1,50 @@
 import React from 'react'
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import AdminNav from './components/AdminNav'
+import AdminSideBar from "./components/AdminSideBar";
+import {useRecoilValue} from "recoil";
+import { userInfo } from '../../atoms'
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+
+const theme = createTheme(
+    {
+        palette: {
+            black: {
+                main: "#212121",
+            },
+        },
+        typography: {
+            fontFamily: [
+                "-apple-system",
+                "SCoreDream",
+                "BlinkMacSystemFont",
+                '"Segoe UI"',
+                "Roboto",
+                '"Helvetica Neue"',
+                "Arial",
+                "sans-serif",
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+            ].join(","),
+        },
+    }
+);
 
 function AdminHome(){
-    return (
-        <React.Fragment>
-            <Container fixed>
-                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+    const adminInfo = useRecoilValue(userInfo)
 
-                </Box>
-            </Container>
-        </React.Fragment>
+    return (
+        <ThemeProvider theme={theme}>
+            <React.Fragment>
+                    <AdminNav adminName={adminInfo.userName}/>
+                    <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+                        <AdminSideBar/>
+                    </Box>
+            </React.Fragment>
+        </ThemeProvider>
     )
 }
 
