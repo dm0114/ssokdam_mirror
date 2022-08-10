0941de6a675640058d529d05caaaacce0ff5dc28
유승우 <swyou1123@naver.com>
2022-08-10 16:53:02 +0900

    inital commit

--- Backend/project/src/test/java/com/ssaft/project/EmbeddedRepositoryTest.java
+++ Backend/project/src/test/java/com/ssaft/project/EmbeddedRepositoryTest.java
@@ -22,10 +22,11 @@ public class EmbeddedRepositoryTest {
     EmbeddedDataRepository embeddedDataRepository;
 
     @Test
-    public void 임베디드qr체크(){
+    public void 임베디드등록(){
         List<EmbeddedData> embeddedData = embeddedDataRepository.findAll();
         for(int i=0;i<embeddedData.size();i++){
             embeddedService.qrCheck(i);
         }
     }
+
 }
