865a14bd8bc8df372e5fc9b3e0da2e5b3ea5598e
유승우 <swyou1123@naver.com>
2022-08-13 14:21:22 +0900

    inital commit

--- Backend/project/src/main/java/com/ssaft/project/Service/UseService.java
+++ Backend/project/src/main/java/com/ssaft/project/Service/UseService.java
@@ -1,5 +1,6 @@
 package com.ssaft.project.Service;
 
+import com.ssaft.project.Function.Function;
 import com.ssaft.project.Repository.EmbeddedDataRepository;
 import com.ssaft.project.Repository.IotUserRepository;
 import com.ssaft.project.Repository.UseDataRepository;
@@ -7,6 +8,9 @@ import com.ssaft.project.domain.UseData;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 public class UseService {
@@ -23,6 +27,9 @@ public class UseService {
     @Autowired
     EmbeddedDataRepository embeddedDataRepository;
 
+    @Autowired
+    Function function;
+
     public void usePush(UseData useData){                  // 담배 판별 'Y' 'N' 에 따라 작업 수행
         if(useData.getUseCheck().equals("Y")){
             iotUserService.cigPush(useData.getUserId());
@@ -33,5 +40,17 @@ public class UseService {
         useData.setEmbeddedData(embeddedDataRepository.findById(useData.getEmbId()).get());
         System.out.println(useData);
         useDataRepository.save(useData);
+    }
+
+    public Map<String, Object>  useCheck(String token){
+        Map<String, Object> map = new LinkedHashMap<>();
+        String id = function.getSubJect(token);
+        if(id.equals("토큰만료")){
+            map.put("ok", "토큰만료");
+            return map;
+        }
+        
+
+        return map;
     }
 }
